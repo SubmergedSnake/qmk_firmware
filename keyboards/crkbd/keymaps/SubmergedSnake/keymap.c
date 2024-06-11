357446f873a3d45cc11ec013e0ea5faf871d303a
Joonas Heikkinen <solarshadow.jh@gmail.com>
2024-06-11 22:51:50 +0300

    define layout as qwerty, let os map to colemak

--- keyboards/crkbd/keymaps/SubmergedSnake/keymap.c
+++ keyboards/crkbd/keymaps/SubmergedSnake/keymap.c
@@ -20,9 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_3(
- KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
-LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),      \
-KC_Z, ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,\
+
+//,-----------------------------------------------------.                    ,-----------------------------------------------------.
+          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+  //|---------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+          LGUI_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    KC_G,                         KC_H,    LSFT_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L), LGUI_T(KC_SCLN),
+  //|---------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                            LT(1,KC_ESC),LT(2,KC_SPC),  LT(3,KC_TAB),LT(4,KC_ENT),  LT(5,KC_BSPC), LT(6,KC_DEL)
 
   ),
