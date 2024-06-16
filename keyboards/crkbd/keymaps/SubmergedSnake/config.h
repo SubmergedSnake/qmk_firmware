e9e8807abf2b847a8e4be32ddfd1b62112dc7a3c
Joonas Heikkinen <solarshadow.jh@gmail.com>
2024-06-16 22:34:06 +0300

    quick tap zero

--- keyboards/crkbd/keymaps/SubmergedSnake/config.h
+++ keyboards/crkbd/keymaps/SubmergedSnake/config.h
@@ -28,10 +28,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-//#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 215
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 220
+
+
 
 #ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_DEFAULT_HUE 64
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
